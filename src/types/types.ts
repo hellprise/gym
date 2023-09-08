01efc46987618bde520a2b36030d2627393f298b
hellprise <mleokha@gmail.com>
2023-09-09 02:50:13 +0300

    initial settings

---
+++ src/types/types.ts
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react'
+
+export type TypesWithClassName<P = unknown> = P & { className?: string | undefined }
+
+export type TypesWithClassesAndChildren<P = unknown> = TypesWithClassName<P> & { children?: ReactNode }
