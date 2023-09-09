5636a5dba436a6f04ddf60c9a85202dcf5cc982c
hellprise <mleokha@gmail.com>
2023-09-10 02:33:11 +0300

    add pages structure for the app

---
+++ src/app/gyms/[slug]/map/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function GymMap() {
+	return <div>GymMap</div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-	return <></>
+	return (
+		<>
+			<h1 className='text-2xl font-bold text-green-300 underline'>Hello world</h1>
+		</>
+	)
 }
